e0dd4bed3f95d31e690b7523db153eead676129b
Johannes Skuterud <johann1301s@icloud.com>
2025-12-06 01:37:22 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,8 @@ export default function Page() {
   const [state, setState] = useState({
     angle: 0,
     rotation: 0,
-    focX: 0,
-    focY: 0,
-    focZ: 0,
-    camX: 50,
-    camY: 50,
-    camZ: 50
+    C: [50, 50, 50] as TVector,
+    O: [0,0,0] as TVector
   })
 
   const ref = useRef<HTMLDivElement>(null)
@@ -23,22 +19,30 @@ export default function Page() {
     if (typeof document !== 'object') return
     ref.current.innerHTML = ''
     for (let i = 0; i < currentShape.length; i++) {
-      const j = state.angle/100
       const p = state.rotation/100
 
-      const angleRad = 2*Math.PI*j
-      const O: TVector = [state.focX, state.focY, state.focZ]
-      const C: TVector = add([0,0, 0], multiply([Math.cos(angleRad), Math.sin(angleRad),0], Math.sqrt(2*125*125)))
-
-      const [X, Y] = projectPoint(currentShape[i], O, C, G, p, 200, 200)
+      const [X, Y] = projectPoint(currentShape[i], state.O, state.C, G, p, 200, 200)
+      
       const point = document.createElement('DIV')
-
       point.className = 'point'
       point.style.left = X + 'px'
       point.style.bottom = Y + 'px'
       ref.current?.appendChild(point)
     }
   }, [state])
+
+  const onAngleChange = (value: string) => {
+    const angle = parseInt(value)
+    const j = angle/100
+    const angleRad = 2*Math.PI*j
+    const C: TVector = add([0,0, 0], multiply([Math.cos(angleRad), Math.sin(angleRad),0], Math.sqrt(2*125*125)))
+
+    setState((p) => ({
+      ...p,
+      C: C,
+      angle: angle
+    }))
+  }
 
   return (
     <Frame>
@@ -52,33 +56,19 @@ export default function Page() {
         Radians XY: {(state.angle/100).toFixed(2)}τ
       </div><br/>
       <Slider
-        onChange={({target}) => setState((p) => ({...p, angle: parseInt(target.value)}))}
+        onChange={({target}) => onAngleChange(target.value)}
         type='range' min={0} max={100} step={1} value={state.angle}/>
       <div>
         Rotation: {(state.rotation/100).toFixed(2)}τ
       </div><br/>
+
       <Slider
         onChange={({target}) => setState((p) => ({...p, rotation: parseInt(target.value)}))}
         type='range' min={0} max={100} step={1} value={state.rotation}/>
-      <div>
-        FocusPoint: [{state.focX}, {state.focY}, {state.focZ}]
-      </div><br/>
-      <input type='number' value={state.focX} onChange={({target}) => setState((p) => ({...p, focX: parseFloat(target.value)}))}/>
-      <br/>
-      <input type='number' value={state.focY} onChange={({target}) => setState((p) => ({...p, focY: parseFloat(target.value)}))}/>
-      <br/>
-      <input type='number' value={state.focZ} onChange={({target}) => setState((p) => ({...p, focZ: parseFloat(target.value)}))}/>
-      <br/>
 
       <div>
-        CameraPoint: [{state.camX}, {state.camY}, {state.camZ}]
-      </div><br/>
-      <input type='number' value={state.camX} onChange={({target}) => setState((p) => ({...p, camX: parseFloat(target.value)}))}/>
-      <br/>
-      <input type='number' value={state.camY} onChange={({target}) => setState((p) => ({...p, camY: parseFloat(target.value)}))}/>
-      <br/>
-      <input type='number' value={state.camZ} onChange={({target}) => setState((p) => ({...p, camZ: parseFloat(target.value)}))}/>
-      <br/>
+        CameraPoint: [{state.C[0].toFixed(2)}, {state.C[1].toFixed(2)}, {state.C[2].toFixed(2)}]
+      </div>
     </Frame>
   )
 }
